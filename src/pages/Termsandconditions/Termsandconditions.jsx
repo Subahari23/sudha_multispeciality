b4278e51f3e393d8fc0d574aa8c00d8e4aaea7b9
Subahari23 <subahari.m@atts.in>
2025-08-21 12:21:53 +0530

    new

--- src/pages/Termsandconditions/Termsandconditions.jsx
+++ src/pages/Termsandconditions/Termsandconditions.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
-import Banner from "@/assets/termsandconditions/banner.png";
+import Banner from "@/assets/termsandconditions/Banner.png";
 import Breadcrumb from "@/components/Breadcrumb";
 
 
